898eb92108f22988011b4a6fa296a40082e2579b
minh <hoangminh160997@gmail.com>
2019-07-21 14:27:57 -0700

    add file

--- algorithm question/coinfield.js
+++ algorithm question/coinfield.js
@@ -1,12 +1,15 @@
 function search(needle = 0, heystack = []) {
     for (let i = 0; i < heystack.length; i++) {
-        if (heystack[i] === needle) {
+        if (Array.isArray(heystack[i])) {
+            return search(needle, heystack[i]);
+        } else if (heystack[i] === needle) {
             return true;
         }
     }
+    return false;
 }
 
-const heystack = [1, 4, [5, 6, 7, [8, 18, [34, 17, [98, [210, [213]]]]]]];
+const heystack = [1, 3, [5, 6, 7, [8, 18, [34, 17, [98, [210, [213]]]]]]];
 const needle = 213;
 
 console.log(search(needle, heystack));
---
+++ algorithm question/tree.js
@@ -0,0 +1,57 @@
+class Node {
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    //Left
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    //Right
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+    lookup(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                return currentNode;
+            }
+        }
+        return null;
+    }
+
+    remove(value) {}
+}
